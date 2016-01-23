2f277fe54e5558a6d97b9d38070b465343cdd6ad
Mathias Ettinger <mathias@mettinger.fr>
2016-01-23 19:46:31 +0100

    Count comments by date. Multi-scale data representation.

--- the_eye.py
+++ the_eye.py
@@ -1,28 +1,50 @@
 import sys
-import math
-import facebook
+import datetime
 import argparse
+from collections import Counter
+import facebook
 from pprint import pformat as pretty_string
-from getpass import getpass
 
 
-COMMENT_PER_QUERY = 999
+COMMENT_PER_QUERY = 1000
+DAYS_COUNT = datetime.timedelta(days=30)
 
 
-def get_secret(secret=None):
-    return secret if secret is not None else getpass()
+class TimeSeries(object):
+    def __init__(self):
+        self.today = datetime.date.today()
+        self.start_observation = self.today - DAYS_COUNT
+        self.hours = Counter()
+        self.days = Counter()
+        self.months = Counter()
+        self.years = Counter()
+
+    def parse_new_time(self, time, format='%Y-%m-%dT%H:%M:%S+0000'):
+        time = datetime.datetime.strptime(time, format)
+        date = time.date()
+        if date == self.today:
+            self.hours[time.hour] += 1
+        if date >= self.start_observation:
+            self.days[date] += 1
+        self.months[date.replace(day=1)] += 1
+        self.years[date.replace(month=1, day=1)] += 1
+
+    def __iter__(self):
+        yield 'Today', self.hours.iteritems()
+        yield 'Last month', self.days.iteritems()
+        yield 'Each month', self.months.iteritems()
+        yield 'Each year', self.years.iteritems()
 
 
 class FacebookComments(object):
-    def __init__(self, token=None, app_id=None, app_secret=None):
-        if token is None:
-            token = facebook.get_app_access_token(app_id, get_secret(app_secret))
+    def __init__(self, token, output):
         self.graph = facebook.GraphAPI(token)
+        self.comments = TimeSeries()
+        self.output = output
 
-    def analyse(self, object_id, output_file):
-        for comment in self._fetch(object_id + '/comments'):
-            output_file.write(comment)
-            output_file.write('\n')
+    def analyze(self, object_id):
+        for comment_time in self._fetch(object_id + '/comments'):
+            self.comments.parse_new_time(comment_time)
 
     def _fetch(self, path):
         response = self.graph.get_object(path, limit=COMMENT_PER_QUERY, summary=True, filter='stream')
@@ -45,6 +67,16 @@ class FacebookComments(object):
                     print 'Error occured (', e, '), stopping after', count, 'comments.'
                     break
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, a, b, c):
+        for event, data in self.comments:
+            self.output.write('{}:\n'.format(event))
+            for date, amount in sorted(data):
+                self.output.write('\t{}:\t{}\n'.format(date, amount))
+        self.output.close()
+
 
 class StringOrStdinAction(argparse.Action):
     def __call__(self, parser, namespace, value, option=None):
@@ -59,6 +91,5 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    comments = FacebookComments(args.token)
-    data = comments.analyse(args.object, args.output)
-    args.output.close()
+    with FacebookComments(args.token, args.output) as parser:
+        parser.analyze(args.object)
