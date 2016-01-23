24ffaac106772accd1a3a25d78fca52b8b1acb9e
Mathias Ettinger <mathias@mettinger.fr>
2016-01-23 15:02:21 +0100

    Parsing dates from comments. Still slow: 6 minutes for example ID

--- the_eye.py
+++ the_eye.py
@@ -9,14 +9,37 @@ def get_secret(secret=None):
     return secret if secret is not None else getpass()
 
 
-def get_app_token(token=None, app_id=None, app_secret=None):
-    return (token
-            if token is not None
-            else facebook.get_app_access_token(
-                    app_id,
-                    get_secret(app_secret)
-                 )
-           )
+class FacebookComments(object):
+    def __init__(self, token=None, app_id=None, app_secret=None):
+        if token is None:
+            token = facebook.get_app_access_token(app_id, get_secret(app_secret))
+        self.graph = facebook.GraphAPI(token)
+
+    def analyse(self, object_id, output_file):
+        for comment in self.fetch_comments(object_id + '/comments'):
+            output_file.write(comment)
+            output_file.write('\n')
+
+    def fetch_comments(self, path):
+        response = self.graph.get_object(path, limit=1000, summary=True, filter='stream')
+        total = response['summary']['total_count']
+        count = 0
+
+        while True:
+            count += len(response['data'])
+            print 'Got', count, 'over', total, 'comments.'
+            for comment in response['data']:
+                yield comment['created_time']
+            try:
+                after = response['paging']['cursors']['after']
+            except KeyError:
+                break
+            else:
+                try:
+                    response = self.graph.get_object(path, limit=1000, after=after, filter='stream')
+                except facebook.GraphAPIError as e:
+                    print 'Error occured (', e, '), stopping after', count, 'comments.'
+                    break
 
 
 if __name__ == '__main__':
@@ -27,10 +50,7 @@ if __name__ == '__main__':
     parser.add_argument('token', nargs='?')
 
     args = parser.parse_args()
-    token = get_app_token(args.token, args.app_id, args.app_secret)
 
-    graph = facebook.GraphAPI(token)
-    obj = graph.get_object('10151775534413086')
-
-    args.output.write(pretty_string(obj))
+    comments = FacebookComments(args.token, args.app_id, args.app_secret)
+    data = comments.analyse('10151775534413086', args.output)
     args.output.close()
